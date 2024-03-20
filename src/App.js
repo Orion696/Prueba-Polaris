7cf5d9d59fafe75c80d93ff82cb4bf0763a1e3df
Orion696 <sanandres557@gmail.com>
2024-03-20 12:15:21 -0500

    Ajustes visiuales y css para mejorar experiencia de usuario

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {AppProvider} from '@shopify/polaris';
 import Layout from './components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,12 +6,12 @@ import PolarisButton from './PolarisButton';
 import CentralContent from './CentralContent';
 import StyleChanger from './StyleChanger';
 import useWindowSize from './useWindowSize';
-import Acordeon from './Acordeon';
 
 const Layout = () => {
   const windowSize = useWindowSize();
   const [showRightColumn, setShowRightColumn] = useState(false);
   const [acordeonOpen, setAcordeonOpen] = useState(false);
+  const [isAcordeonOptionVisible, setIsAcordeonOptionVisible] = useState({ back: false, styleChanger: false });
 
   const handleCentralButtonClick = () => {
     if (windowSize.width > 333) {
@@ -19,50 +19,85 @@ const Layout = () => {
     }
   };
 
-  const handleBackButtonClick = () => {
-    if (windowSize.width <= 333) {
-      setAcordeonOpen(false);
-    } else {
-      setShowRightColumn(false);
-    }
+  const toggleAcordeonOption = (option) => {
+    setIsAcordeonOptionVisible((prevState) => ({
+      ...{ back: false, styleChanger: false },
+      [option]: !prevState[option],
+    }));
+    setAcordeonOpen(true);
   };
 
   return (
     <Page fullWidth>
       <div className="container-fluid">
         <div className="row">
-          <div className="col left-column">
-            {windowSize.width > 333 && (
-              <PolarisButton onClick={handleBackButtonClick}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <span style={{ marginRight: '30px' }}>&lt;</span> Atrás
+          {windowSize.width <= 333 ? (
+            <div className="col">
+              <CentralContent onClick={handleCentralButtonClick} />
+              <div>
+                <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                        onClick={() => setAcordeonOpen(!acordeonOpen)}
+                      >
+                        Menú
+                      </button>
+                    </h2>
+                    {acordeonOpen && (
+                      <div
+                        id="collapseOne"
+                        className="accordion-collapse collapse show"
+                        aria-labelledby="headingOne"
+                      >
+                        <div className="accordion-body">
+                          <PolarisButton onClick={() => toggleAcordeonOption('back')} style={{ marginRight: '30px' }}>
+                            &lt; Atrás
+                          </PolarisButton>
+                          {isAcordeonOptionVisible.back}
+                          <PolarisButton onClick={() => toggleAcordeonOption('styleChanger')}>StyleChanger</PolarisButton>
+                          {isAcordeonOptionVisible.styleChanger && <StyleChanger />}
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </PolarisButton>
-            )}
-            {windowSize.width > 333 && windowSize.width <= 990 && showRightColumn && (
-              <>
-                <div className="separator-line"></div>
-                <StyleChanger />
-              </>
-            )}
-          </div>
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className="col left-column container">
+                {windowSize.width > 333 && (
+                  <PolarisButton onClick={() => setShowRightColumn(false)}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <span style={{ marginRight: '30px' }}>&lt;</span> Atrás
+                    </div>
+                  </PolarisButton>
+                )}
+                {windowSize.width > 333 && windowSize.width <= 990 && showRightColumn && (
+                  <>
+                    <div className="separator-line"></div>
+                    <StyleChanger />
+                  </>
+                )}
+              </div>
 
-          <div className={windowSize.width > 990 ? 'col-6 central-column' : 'col'}>
-            <CentralContent onClick={handleCentralButtonClick} />
-            {windowSize.width <= 333 && (
-              <Acordeon isOpen={acordeonOpen} setIsOpen={setAcordeonOpen}>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}>
-                  <PolarisButton onClick={handleBackButtonClick} style={{ width: '100%', justifyContent: 'center' }}>{"< Atrás"}</PolarisButton>
+              <div className={windowSize.width > 990 ? 'col-6 central-column container' : 'col container'}>
+                <CentralContent onClick={handleCentralButtonClick} />
+              </div>
+
+              {showRightColumn && windowSize.width > 990 && (
+                <div className="col-3 right-column container">
                   <StyleChanger />
                 </div>
-              </Acordeon>
-            )}
-          </div>
-
-          {showRightColumn && windowSize.width > 990 && (
-            <div className="col-3 right-column">
-              <StyleChanger />
-            </div>
+              )}
+            </>
           )}
         </div>
       </div>
